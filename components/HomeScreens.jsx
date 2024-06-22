5400a9af48db26a4f1214e6dd4d9f5834a6bba30
athabarokahfc <athabarokahfc@gmail.com>
2024-06-22 02:43:39 +0000

    create : weater stat UI

--- components/HomeScreens.jsx
+++ components/HomeScreens.jsx
@@ -12,6 +12,7 @@ import React, { useState } from "react";
 import { opacity } from "../utils/opacity";
 import * as Icons from "react-native-heroicons/outline";
 import * as IconsSolid from "react-native-heroicons/solid";
+import { ScrollView } from "react-native-gesture-handler";
 
 const HomeScreens = () => {
   const [showSearch, setShowSearch] = useState(false);
@@ -30,7 +31,9 @@ const HomeScreens = () => {
         source={require("../assets/images/bg.png")}
       />
       <SafeAreaView className="flex flex-1 my-4 mx-2">
-        <View style={{ height: "7%" }} className="mx-4 relative z-50">
+        {/* Search Section */}
+        <View style={{ height: "7%" }} className="mx-4 mb-14 relative z-50">
+          {/* Search Bar */}
           <View
             className="flex-row justify-end items-center rounded-full"
             style={{
@@ -56,7 +59,7 @@ const HomeScreens = () => {
               <Icons.MagnifyingGlassIcon color={"white"} size={25} />
             </TouchableOpacity>
           </View>
-
+          {/* Search Results */}
           <View>
             {location.length > 0 && showSearch ? (
               <View className="absolute w-full bg-gray-300 top-1 rounded-3xl p-1">
@@ -74,7 +77,7 @@ const HomeScreens = () => {
                       <IconsSolid.MapPinIcon color={"gray"} size={20} />
 
                       <Text className="text-black text-lg ">
-                        London, United Kingdom
+                        Belitung, Indonesia
                       </Text>
                     </TouchableOpacity>
                   );
@@ -82,6 +85,68 @@ const HomeScreens = () => {
               </View>
             ) : null}
           </View>
+        </View>
+        {/* Weather Forecast Section */}
+        <View className="flex  flex-1 mb-2 gap-6 ">
+          {/* Loc Name */}
+          <View>
+            <Text className="text-white text-center text-3xl font-extrabold">
+              Belitung
+              <Text className="text-gray-300 text-center text-lg font-bold">
+                , Bangka Belitung
+              </Text>
+            </Text>
+          </View>
+          {/* Image ForeCasting */}
+          <View className="flex-row justify-center items-center ">
+            <Image
+              className="w-52 h-52"
+              source={require("../assets/images/partlycloudy.png")}
+            />
+          </View>
+          {/* Degree forecast */}
+          <View className="flex-1 items-center ">
+            <Text className="font-extrabold text-white text-6xl ">
+              23&#176;C
+            </Text>
+            <Text className="font-semibold text-white text-xl tracking-widest">
+              Partly Cloudy
+            </Text>
+          </View>
+          {/* Weather Stats */}
+          <View className="flex-row mx-4 justify-around">
+            <View className="flex-row space-x-2  items-center ">
+              <Image
+                className="h-8 w-8"
+                source={require("../assets/icons/wind.png")}
+              />
+              <Text className="text-white font-semibold text-base">22km/h</Text>
+            </View>
+            <View className="flex-row space-x-2  items-center ">
+              <Image
+                className="h-8 w-8"
+                source={require("../assets/icons/drop.png")}
+              />
+              <Text className="text-white font-semibold text-base">23%</Text>
+            </View>
+            <View className="flex-row space-x-2  items-center ">
+              <Image
+                className="h-8 w-8"
+                source={require("../assets/icons/sun.png")}
+              />
+              <Text className="text-white font-semibold text-base">
+                6:05 AM
+              </Text>
+            </View>
+          </View>
+          {/* Daily Forecast */}
+          <View className="flex-row justify-around"></View>
+          <ScrollView
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{ paddingHorizontal: 15 }}
+          >
+            <View className=""></View>
+          </ScrollView>
         </View>
       </SafeAreaView>
     </View>
